001f7335fb11d8cc6021052a4e7353b6f992c984
sray375 <sray15@fordham.edu>
2025-03-06 17:25:32 -0500

    Create Post.php

---
+++ inclass15/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+class Post {
+    public function getPosts() {
+        return [
+            ["title" => "First Post", "description" => "This is the first post description."],
+            ["title" => "Second Post", "description" => "This is the second post description."],
+            ["title" => "Third Post", "description" => "This is the third post description."]
+        ];
+    }
+}
